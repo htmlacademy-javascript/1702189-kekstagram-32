4be95c7f57f96ac2a6f62be484c9b43fd660bf93
Keks <keks@htmlacademy.ru>
2024-06-23 19:07:08 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/functions.js
@@ -0,0 +1,24 @@
+const checkStringLength = (string, length) => {
+  return string.length <= length;
+};
+
+const checkPalindrome = (string) => {
+  const normalizedString = string.toLowerCase().replaceAll(' ', '');
+  let reversedString = '';
+  for (let i = normalizedString.length; i > 0; i--) {
+    reversedString += normalizedString[i - 1];
+  }
+  return reversedString === normalizedString;
+};
+
+const getNumber = (string) => {
+  let result = '';
+  const normalizedString = String(string).replaceAll(' ', '');
+  for (let i = 0; i < normalizedString.length; i++) {
+    const char = Number(normalizedString[i]);
+    if (char || char === 0) {
+      result += normalizedString[i];
+    }
+  }
+  return parseInt(result);
+};
