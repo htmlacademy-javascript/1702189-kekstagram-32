965c8ed52c33b3eac343e9dc594b97bd9d0b43d4
Keks <keks@htmlacademy.ru>
2024-07-29 22:48:43 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/render.js
@@ -0,0 +1,26 @@
+const postsContainer = document.querySelector('.pictures');
+const postTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderPost = ({id, url, description, likes, comments}, container) => {
+  const post = postTemplate.cloneNode(true);
+
+  post.dataset.id = id;
+  post.querySelector('.picture__img').src = url;
+  post.querySelector('.picture__img').alt = description;
+  post.querySelector('.picture__info .picture__comments').textContent = comments.length;
+  post.querySelector('.picture__info .picture__likes').textContent = likes;
+
+  container.append(post);
+};
+
+const renderPosts = (generatedPosts) => {
+  const container = document.createDocumentFragment();
+
+  generatedPosts.forEach((post) => {
+    renderPost(post, container);
+  });
+
+  postsContainer.append(container);
+};
+
+export {renderPosts, postsContainer};
